adedbc5884ef6e1f8555b04cf998bbfac323d124
Schnedi3 <schnedi3@gmail.com>
2025-01-01 00:22:59 +0100

    Backend: Add some dummy data

--- backend/src/database/db.sql
+++ backend/src/database/db.sql
@@ -22,3 +22,17 @@ CREATE TABLE todo (
   due_date DATE NOT NULL,
   completed BOOLEAN NOT NULL DEFAULT FALSE
 );
+
+INSERT INTO project (name, color)
+VALUES
+('Study', '#e02655'),
+('Work', '#7b32c9'),
+('Personal', '#37c932'),
+('Home', '#e3b22b');
+
+INSERT INTO todo (title, description, project_id, priority, due_date)
+VALUES
+('Clone todoist app', 'Use react native', 1, 'high', '2025-01-20'),
+('Go to work on train', 'Go to the station', 2, 'medium', '2025-02-08'),
+('Go to the gym', 'Train for around 60min', 3, 'low', '2025-01-20'),
+('Clean the house', 'Vacuum, sweep, mop', 4, 'medium', '2025-03-15');
