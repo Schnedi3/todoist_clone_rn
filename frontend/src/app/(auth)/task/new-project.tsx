cafa38829bc215bfb50f5f36d38b3428c78d4bb2
Schnedi3 <schnedi3@gmail.com>
2025-01-01 22:20:34 +0100

    Frontend: Implement "new project" page

--- frontend/src/app/(auth)/task/new-project.tsx
+++ frontend/src/app/(auth)/task/new-project.tsx
@@ -1,10 +1,20 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Pressable, StyleSheet, TextInput, View } from "react-native";
 import { Stack } from "expo-router";
+import ColorPicker, { Panel3, Preview } from "reanimated-color-picker";
+import { Ionicons } from "@expo/vector-icons";
 
 import { NewHeader } from "@/src/components/task/NewHeader";
 import { Colors } from "@/src/constants/Colors";
 
 export default function NewProject() {
+  const [name, setName] = useState<string>("");
+  const [color, setColor] = useState<string>("");
+
+  const onSelectColor = ({ hex }: { hex: string }) => {
+    setColor(hex);
+  };
+
   return (
     <>
       <Stack.Screen
@@ -15,7 +25,33 @@ export default function NewProject() {
       />
 
       <View style={styles.container}>
-        <Text style={styles.title}>New project</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Project name"
+            placeholderTextColor={Colors.disabled}
+            style={styles.input}
+            value={name}
+            onChangeText={(text) => setName(text)}
+          />
+
+          {name && (
+            <Pressable
+              style={({ pressed }) => [{ opacity: pressed ? 0.5 : 1 }]}
+              onPress={() => setName("")}
+            >
+              <Ionicons name="close-outline" style={styles.clearIcon} />
+            </Pressable>
+          )}
+        </View>
+
+        <ColorPicker
+          value="#ffffff"
+          onComplete={onSelectColor}
+          style={styles.colorPicker}
+        >
+          <Panel3 />
+          <Preview style={styles.previewStyle} hideInitialColor />
+        </ColorPicker>
       </View>
     </>
   );
@@ -24,12 +60,37 @@ export default function NewProject() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 30,
+    gap: 50,
   },
-  title: {
-    fontFamily: "QuicksandBold",
-    fontSize: 50,
-    color: Colors.disabled,
+  // input
+  inputContainer: {
+    paddingVertical: 16,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    borderRadius: 10,
+    backgroundColor: Colors.primaryBg,
+    boxShadow:
+      "0px 2px 3px -1px rgba(0, 0, 0, 0.1), 0px 2px 3px -1px rgba(0, 0, 0, 0.06)",
+  },
+  input: {
+    flex: 1,
+    fontFamily: "QuicksandMedium",
+    fontSize: 16,
+    color: Colors.primaryText,
+  },
+  clearIcon: {
+    fontSize: 24,
+    color: Colors.secondaryText,
+  },
+  // color picker
+  colorPicker: {
+    gap: 20,
+  },
+  previewStyle: {
+    height: 60,
+    borderRadius: 10,
   },
 });
